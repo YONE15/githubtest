c96e153b233d796a66086cf303b80af150d527a8
DESKTOP-VLN4SUU\vantan <yonemaru15@gmail.com>
2021-08-20 13:14:34 +0900

    キューブとスクリプトを追加

---
+++ Assets/kksk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kksk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("‰Ô‚æ‚èŒs‚Ì•û‚ª‚¤‚Ü‚¢");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
